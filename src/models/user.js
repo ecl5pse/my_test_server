876036cb21882f8e2161a9d14691e402aa229e99
ecl5pse <fantamas20@gmail.com>
2020-02-11 11:59:19 +0200

    update model

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,36 @@
 'use strict';
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique:true,
+    },
+    login:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      unique:true
+
+    },
+    password:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      field:'passwordHash',
+      set(val) {
+        this.setDataValue('password',bcrypt.hashSync(val,10));
+      }
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
